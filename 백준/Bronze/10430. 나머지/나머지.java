07f6b89876bc8ee320ba26d72cce56ffd8244060
chunsik <123478243+haaging@users.noreply.github.com>
2023-11-02 18:45:08 +0900

    [Bronze V] Title: 나머지, Time: 220 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		//if((a >=2 || a <= 10000) && (b >=2 || b <= 10000) && (c >=2 || c <= 10000)) {
+			System.out.println((a+b)%c);
+			System.out.println(((a%c)+(b%c))%c);
+			System.out.println((a*b)%c);
+			System.out.println(((a%c)*(b%c))%c);
+		//}
+		
+		
+		sc.close();
+			
+	}
+	
+}
