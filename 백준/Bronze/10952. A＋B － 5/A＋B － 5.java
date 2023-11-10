5d898eb3672ce17ad5f192c6988800d5eabf6eb1
chunsik <123478243+haaging@users.noreply.github.com>
2023-11-10 23:58:57 +0900

    [Bronze V] Title: A+B - 5, Time: 116 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -1,33 +1,21 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class Main
-{	
-	public static void main(String[] args) throws IOException
-	{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-		
-		StringTokenizer st;
-		
-		
-		while(true)
-		{
-			st = new StringTokenizer(br.readLine());
-			int A = Integer.parseInt(st.nextToken());
-			int B = Integer.parseInt(st.nextToken());
-			
-			if(A==0 && B==0) 
-				break;
-			
-			sb.append(A+B).append("\n");
-		}
-		
-		bw.write(sb.toString());
-		br.close();
-		bw.flush();
-		bw.close();
-	}
-	
+public class Main {
+    public static void main(String[] args)  throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        
+        while (true) {
+            st = new StringTokenizer(br.readLine());            
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int sum = a + b;
+            if (sum == 0) break;
+            sb.append(sum).append("\n");                        
+        }
+        System.out.print(sb);
+        br.close();
+    }
 }
