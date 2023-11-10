172d0147dd0ea1508c66ac4611e75c5f6b365243
chunsik <123478243+haaging@users.noreply.github.com>
2023-11-10 23:16:05 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 124 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+public class Main
+{	
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= N; i++)
+		{
+			for (int j = N-i; j > 0; j--)
+			{
+				sb.append(" ");
+			}
+			for (int j = 1; j <= i; j++)
+			{
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+		
+		bw.write(sb.toString());
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+	
+}
