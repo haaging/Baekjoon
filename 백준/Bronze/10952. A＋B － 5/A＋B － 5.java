85322a6fe21cc0decb496f8047601573903723dc
chunsik <123478243+haaging@users.noreply.github.com>
2023-11-11 00:08:08 +0900

    [Bronze V] Title: A+B - 5, Time: 116 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -1,21 +1,28 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
-public class Main {
-    public static void main(String[] args)  throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
-        
-        while (true) {
-            st = new StringTokenizer(br.readLine());            
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int sum = a + b;
-            if (sum == 0) break;
-            sb.append(sum).append("\n");                        
-        }
-        System.out.print(sb);
-        br.close();
-    }
+public class Main
+{	
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		
+		String input;
+		while(true)
+		{
+			input = br.readLine();
+			int a = (input.charAt(0) - 48);
+			int b = (input.charAt(2) - 48);
+			int sum = a + b;
+			if(sum ==0) 
+				break;
+			
+			sb.append(sum).append("\n");
+		}
+		System.out.println(sb);
+		br.close();
+	}
+	
 }
